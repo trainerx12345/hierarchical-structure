0c009b5fe20f9780768be31c629e031ad4388240
sam <sambusinesstransac@gmail.com>
2023-03-22 13:34:43 +0800

    feat: add files

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace hierarchical_structure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
