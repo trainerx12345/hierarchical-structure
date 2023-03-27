eccd4f54a9f6ea163fa8d34c445261286b3271c4
sam <sambusinesstransac@gmail.com>
2023-03-27 12:29:40 +0800

    feat: changes

--- Program.cs
+++ Program.cs
@@ -6,24 +6,38 @@
         {
             Branch rootBranch = new Branch(1);
 
-            Branch childBranch1 = new Branch(2);
-            rootBranch.AddChild(childBranch1);
+            Branch leftBranch = new Branch(2);
+            rootBranch.AddChild(leftBranch);
 
-            Branch childBranch2 = new Branch(3);
-            rootBranch.AddChild(childBranch2);
+            Branch rightBranch = new Branch(3);
+            rootBranch.AddChild(rightBranch);
 
-            Branch grandChildBranch1 = new Branch(4);
-            childBranch1.AddChild(grandChildBranch1);
+            Branch thirdDepth1 = new Branch(4);
+            leftBranch.AddChild(thirdDepth1);
 
-            Branch grandChildBranch2 = new Branch(5);
-            childBranch1.AddChild(grandChildBranch2);
+            Branch thirdDepth2 = new Branch(5);
+            rightBranch.AddChild(thirdDepth2);
 
-            Branch grandChildBranch3 = new Branch(6);
-            childBranch2.AddChild(grandChildBranch3);
+            Branch thirdDepth3 = new Branch(6);
+            rightBranch.AddChild(thirdDepth3);
 
-            Branch grandChildBranch4 = new Branch(7);
-            childBranch2.AddChild(grandChildBranch4);
+            Branch thirdDepth4 = new Branch(7);
+            rightBranch.AddChild(thirdDepth4);
 
+            Branch fourthDepth1 = new Branch(8);
+            thirdDepth2.AddChild(fourthDepth1);
+
+            Branch fourthDepth2 = new Branch(9);
+            thirdDepth3.AddChild(fourthDepth2);
+
+            Branch fourthDepth3 = new Branch(10);
+            thirdDepth3.AddChild(fourthDepth3);
+
+            Branch fithDepth1 = new Branch(11);
+            fourthDepth1.AddChild(fithDepth1);
+
+            rootBranch.PrintBranch();
+            Console.ReadLine();
         }
 
         public class Branch
@@ -40,6 +54,13 @@
             public void AddChild(Branch child)
             {
                 branches.Add(child);
+            }
+            public void PrintBranch() { 
+               foreach (Branch child in branches)
+                {
+                    Console.WriteLine($"{child.Value}");  
+       
+                }
             }
         }
 
